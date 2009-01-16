0ec39d8988f06fef97fa16c7f0d922493f847164
gt67 <gt67@6247ab75-7297-438c-a817-b4e4d5db8f37>
2009-01-16 08:33:04 +0000

    Made the gtk-media-stop icon to revert to the gtk-media-play one when the preview is ended.
    git-svn-id: https://svn.code.sf.net/p/imagination/code@70 6247ab75-7297-438c-a817-b4e4d5db8f37

--- src/callbacks.c
+++ src/callbacks.c
@@ -361,15 +361,7 @@ void img_start_stop_preview(GtkButton *button, img_window_struct *img)
 			g_source_remove(img->source_id);
 			img->source_id = -1;
 		}
-		g_signal_handlers_disconnect_by_func(img->image_area,img_on_expose_event,NULL);
-		tmp_image = gtk_image_new_from_stock ("gtk-media-play",GTK_ICON_SIZE_MENU);
-		gtk_image_menu_item_set_image (GTK_IMAGE_MENU_ITEM (img->preview_menu),tmp_image);
-	
-		tmp_image = gtk_image_new_from_stock ("gtk-media-play",3);
-		gtk_widget_show(tmp_image);
-		g_object_set(img->preview_button,"icon-widget",tmp_image,NULL);
-		gtk_widget_set_tooltip_text(img->preview_button, _("Starts the preview"));
-		return;
+		goto here;
 	}
 	tmp_image = gtk_image_new_from_stock ("gtk-media-stop",GTK_ICON_SIZE_MENU);
 	gtk_image_menu_item_set_image (GTK_IMAGE_MENU_ITEM (img->preview_menu),tmp_image);
@@ -411,10 +403,17 @@ void img_start_stop_preview(GtkButton *button, img_window_struct *img)
 		img->pixbuf2 = img_scale_pixbuf(img,entry->filename);
 		img_idle_function(entry->duration,img);
 	}
-
-	img->preview_is_running = FALSE;
-	gtk_widget_set_app_paintable(img->image_area, FALSE);
+here:
 	g_signal_handlers_disconnect_by_func(img->image_area,img_on_expose_event,NULL);
+	gtk_widget_set_app_paintable(img->image_area, FALSE);
+	tmp_image = gtk_image_new_from_stock ("gtk-media-play",GTK_ICON_SIZE_MENU);
+	gtk_image_menu_item_set_image (GTK_IMAGE_MENU_ITEM (img->preview_menu),tmp_image);
+	
+	tmp_image = gtk_image_new_from_stock ("gtk-media-play",3);
+	gtk_widget_show(tmp_image);
+	g_object_set(img->preview_button,"icon-widget",tmp_image,NULL);
+	gtk_widget_set_tooltip_text(img->preview_button, _("Starts the preview"));
+	img->preview_is_running = FALSE;
 }
 
 static gboolean img_on_expose_event(GtkWidget *widget,GdkEventExpose *event,img_window_struct *img)
