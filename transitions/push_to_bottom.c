0debf2fe2cb57d1321a3a7f0e61324b39172a6e9
gt67 <gt67@6247ab75-7297-438c-a817-b4e4d5db8f37>
2009-01-22 14:32:28 +0000

    Added "Push to Bottom" transition; still to be completed.
    git-svn-id: https://svn.code.sf.net/p/imagination/code@94 6247ab75-7297-438c-a817-b4e4d5db8f37

---
+++ transitions/push_to_bottom.c
@@ -0,0 +1,50 @@
+/*
+ *  Copyright (c) 2009 Giuseppe Torelli <colossus73@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License,or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not,write to the Free Software
+ *  Foundation,Inc.,59 Temple Place - Suite 330,Boston,MA 02111-1307,USA.
+ *
+ */
+
+#include <gdk/gdk.h>
+
+void img_transition_set_name(gchar **name)
+{
+	*name = "Push To Bottom";
+}
+
+void img_transition_render(GdkDrawable *window, GdkPixbuf *image_from, GdkPixbuf *image_to, gdouble progress)
+{
+	cairo_t *cr;
+	gint     x,y, width, height;
+
+	gdk_drawable_get_size(window, &width, &height);
+	x = (width  - gdk_pixbuf_get_width (image_from)) / 2;
+	y = (height - gdk_pixbuf_get_height(image_from)) / 2;
+
+	y *= progress;
+
+	cr = gdk_cairo_create(window);
+	gdk_cairo_set_source_pixbuf(cr,image_from,x,y);
+	cairo_paint(cr);
+
+	x = (width  - gdk_pixbuf_get_width (image_to)) / 2;
+	y = 1024;
+	
+	y *= (1 - progress);
+	gdk_cairo_set_source_pixbuf(cr,image_to,x,y);
+
+	cairo_paint(cr);
+	cairo_destroy(cr);
+}
