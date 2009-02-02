e15b2e164a565110e08af20339114d41a4258e29
gt67 <gt67@6247ab75-7297-438c-a817-b4e4d5db8f37>
2009-02-02 11:07:41 +0000

    Added "Push from left" and "Push from right" transitions.
    git-svn-id: https://svn.code.sf.net/p/imagination/code@108 6247ab75-7297-438c-a817-b4e4d5db8f37

---
+++ transitions/push_from_left.c
@@ -0,0 +1,42 @@
+/*
+ *  Copyright (c) 2009 Giuseppe Torelli <colossus73@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License,or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not,write to the Free Software
+ *  Foundation,Inc.,59 Temple Place - Suite 330,Boston,MA 02111-1307,USA.
+ *
+ */
+
+#include <gdk/gdk.h>
+
+void img_transition_set_name(gchar **name)
+{
+	*name = "Push From Left To Right";
+}
+
+void img_transition_render(GdkDrawable *window, GdkPixbuf *image_from, GdkPixbuf *image_to, gdouble progress)
+{
+	cairo_t *cr;
+	gint	width, height;
+
+	gdk_drawable_get_size(window, &width, &height);
+
+	cr = gdk_cairo_create(window);
+	gdk_cairo_set_source_pixbuf(cr,image_to,-width * (1- progress),0);
+	cairo_paint(cr);
+
+	gdk_cairo_set_source_pixbuf(cr,image_from,width * progress,0);
+
+	cairo_paint(cr);
+	cairo_destroy(cr);
+}
