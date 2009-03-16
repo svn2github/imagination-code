9480716ff54419ee8a8da9ed30717db3b995f2ee
gt67 <gt67@6247ab75-7297-438c-a817-b4e4d5db8f37>
2009-03-16 14:59:14 +0000

    When the play ends Imagination now swaps the audio buttons images. Added ability to recognize audio file types.
    git-svn-id: https://svn.code.sf.net/p/imagination/code@174 6247ab75-7297-438c-a817-b4e4d5db8f37

--- src/audio.c
+++ src/audio.c
@@ -19,6 +19,10 @@
 
 #include "audio.h"
 
+static gchar *img_get_audio_filetype(gchar *);
+static void img_play_audio_ended (GPid ,gint ,img_window_struct *);
+static void img_swap_audio_files_button(img_window_struct *, gboolean );
+
 	/*
 		dialog = gtk_message_dialog_new(GTK_WINDOW(img->imagination_window),GTK_DIALOG_MODAL,GTK_MESSAGE_ERROR,GTK_BUTTONS_OK,_("Can't read file header!"));
 		gtk_message_dialog_format_secondary_text (GTK_MESSAGE_DIALOG (dialog),"%s.",strerror(errno));
@@ -28,20 +32,27 @@
 		return NULL;
 	}*/
 
+static gchar *img_get_audio_filetype(gchar *filename)
+{
+	if (g_str_has_suffix(filename, ".mp3") || g_str_has_suffix(filename, ".MP3"))
+		return "mp3";
+	else if (g_str_has_suffix(filename, ".ogg") || g_str_has_suffix(filename, ".OGG"))
+		return "ogg";
+	else if (g_str_has_suffix(filename, ".flac") || g_str_has_suffix(filename, ".FLAC"))
+		return "flac";
+	else if (g_str_has_suffix(filename, ".wav") || g_str_has_suffix(filename, ".WAV"))
+		return "wav";
+
+	return NULL;
+}
+
 gchar *img_get_audio_length(img_window_struct *img, gchar *filename, gint *secs)
 {
 	gint seconds = -1;
 	gchar *filetype;
 	sox_format_t *ft;
 
-	if (g_str_has_suffix(filename, ".mp3") || g_str_has_suffix(filename, ".MP3"))
-		filetype = "mp3";
-	else if (g_str_has_suffix(filename, ".ogg") || g_str_has_suffix(filename, ".OGG"))
-		filetype = "ogg";
-	else if (g_str_has_suffix(filename, ".flac") || g_str_has_suffix(filename, ".FLAC"))
-		filetype = "flac";
-	else if (g_str_has_suffix(filename, ".wav") || g_str_has_suffix(filename, ".WAV"))
-		filetype = "wav";
+	filetype = img_get_audio_filetype(filename);
 
 	sox_format_init();
 	ft = sox_open_read(filename, NULL, NULL, filetype);
@@ -64,17 +75,11 @@ void img_play_stop_selected_file(GtkButton *button, img_window_struct *img)
 	gboolean ret;
 	GtkTreeModel *model;
 	GtkTreeIter iter;
-	GtkWidget *tmp_image;
 
 	if (img->play_child_pid)
 	{
 		kill (img->play_child_pid, SIGINT);
-		img->play_child_pid = 0;
-		
-		tmp_image = gtk_image_new_from_stock (GTK_STOCK_MEDIA_PLAY,GTK_ICON_SIZE_MENU);
-		gtk_button_set_image(GTK_BUTTON(img->play_audio_button), tmp_image);
-		gtk_widget_set_tooltip_text(img->play_audio_button, _("Play the selected file"));
-		gtk_statusbar_pop(GTK_STATUSBAR(img->statusbar), img->context_id);
+		img_swap_audio_files_button(img, TRUE);
 		return;
 	}
 	gtk_tree_selection_get_selected(gtk_tree_view_get_selection(GTK_TREE_VIEW(img->music_file_treeview)), &model, &iter);
@@ -85,21 +90,49 @@ void img_play_stop_selected_file(GtkButton *button, img_window_struct *img)
 	g_free(filename);
 
 	/* TODO get the correct file type */
-	cmd_line = g_strconcat("play -t mp3 ", file, NULL);
+	cmd_line = g_strconcat("play -t ", img_get_audio_filetype(file), " ", file, NULL);
 	g_print ("%s\n",cmd_line);
 
 	argv = g_strsplit(cmd_line," ", 0);
 	g_free(cmd_line);
 	ret = g_spawn_async_with_pipes( NULL, argv, NULL,
-									G_SPAWN_SEARCH_PATH | G_SPAWN_STDOUT_TO_DEV_NULL | G_SPAWN_STDERR_TO_DEV_NULL,
+									G_SPAWN_SEARCH_PATH | 
+									G_SPAWN_DO_NOT_REAP_CHILD | 
+									G_SPAWN_STDOUT_TO_DEV_NULL | 
+									G_SPAWN_STDERR_TO_DEV_NULL,
 									NULL, NULL, &img->play_child_pid, NULL, NULL, NULL, &error );
 									
+	g_child_watch_add(img->play_child_pid, (GChildWatchFunc) img_play_audio_ended, img);
+
+	img_swap_audio_files_button(img, FALSE);
 	message = g_strdup_printf(_("Playing %s..."),file);
 	gtk_statusbar_push(GTK_STATUSBAR(img->statusbar), img->context_id, message);
 	g_free(file);
 	g_free(message);
-
-	tmp_image = gtk_image_new_from_stock (GTK_STOCK_MEDIA_STOP,GTK_ICON_SIZE_MENU);
-	gtk_button_set_image(GTK_BUTTON(img->play_audio_button), tmp_image);
-	gtk_widget_set_tooltip_text(img->play_audio_button, _("Stop the playback"));
 }
+
+static void img_play_audio_ended (GPid pid, gint status, img_window_struct *img)
+{
+	img_swap_audio_files_button (img, TRUE);
+}
+
+static void img_swap_audio_files_button(img_window_struct *img, gboolean flag)
+{
+	GtkWidget *tmp_image;
+
+	if (flag)
+	{
+		tmp_image = gtk_image_new_from_stock (GTK_STOCK_MEDIA_PLAY,GTK_ICON_SIZE_MENU);
+		gtk_button_set_image(GTK_BUTTON(img->play_audio_button), tmp_image);
+		gtk_widget_set_tooltip_text(img->play_audio_button, _("Play the selected file"));
+		gtk_statusbar_pop(GTK_STATUSBAR(img->statusbar), img->context_id);
+		img->play_child_pid = 0;
+	}
+	else
+	{
+		tmp_image = gtk_image_new_from_stock (GTK_STOCK_MEDIA_STOP,GTK_ICON_SIZE_MENU);
+		gtk_button_set_image(GTK_BUTTON(img->play_audio_button), tmp_image);
+		gtk_widget_set_tooltip_text(img->play_audio_button, _("Stop the playback"));
+	}
+}
+	
